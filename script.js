8491aa39696c98d719d8b352ea5c056c1b14b267
sneha thakur <thakursnehalodhi@gmail.com>
2023-12-28 17:59:06 +0530

    first commit

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
         var navlinks = document.getElementById("nav-links");
         function ShowMenue(){
             navlinks.style.right= "0";
-            // navlinks.style.display= "block";
+            navlinks.style.display= "block";
+            // navlinks.style.transition= "1s";
         }
         function HideMenue(){
-            navlinks.style.right= "-51%";
+            navlinks.style.display= "none";
+            // navlinks.style.transition= "1s";
             // navlinks.style.display= "none";
         }
